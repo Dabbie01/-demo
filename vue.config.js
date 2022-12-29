acae6ee85d359f42032c7c8c199837529c37c8c5
Dabbie <2310734576@qq.com>
2022-12-29 16:51:42 +0800

    解决跨域问题

--- vue.config.js
+++ vue.config.js
@@ -35,6 +35,14 @@ module.exports = {
     overlay: {
       warnings: false,
       errors: true
+    },
+    // 代理跨域的配置，配置反向代理
+    proxy: {
+      // 当我们的本地的请求 有/api的时候，就会代理我们的请求地址向另外一个服务器发出请求
+      '/api': {
+        target: 'http://ihrm-java.itheima.net/', // 跨域请求的地址
+        changeOrigin: true // 是否跨域 只有这个值为true的情况下 才表示开启跨域
+      }
     }
   },
   configureWebpack: {
