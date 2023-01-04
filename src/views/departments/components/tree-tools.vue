59c989dc21f0a1968848ada9e0feeeee6e40207c
Dabbie <2310734576@qq.com>
2023-01-04 17:35:38 +0800

    封装单独树形组织结构

---
+++ src/views/departments/components/tree-tools.vue
@@ -0,0 +1,55 @@
+<template>
+  <el-row
+    type="flex"
+    justify="space-between"
+    align="middle"
+    style="height: 40px; width: 100%"
+  >
+    <!-- 左侧内容 -->
+    <el-col>
+      <span>{{ treeNode.name }}</span>
+    </el-col>
+    <!-- 右侧内容 -->
+    <el-col :span="4">
+      <el-row type="flex" justify="end">
+        <el-col>{{ treeNode.manager }}</el-col>
+        <el-col>
+          <!-- 放置下拉菜单 -->
+          <el-dropdown>
+            <!-- 内容 -->
+            <span>操作
+              <i class="el-icon-arrow-down" />
+            </span>
+            <!-- 具名插槽 -->
+            <el-dropdown-menu slot="dropdown">
+              <!-- 下拉选项 -->
+              <el-dropdown-item>添加子部门</el-dropdown-item>
+              <el-dropdown-item v-if="!isRoot">编辑部门</el-dropdown-item>
+              <el-dropdown-item v-if="!isRoot">删除部门</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </el-col>
+      </el-row>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  // props可以用数组来接收数据 也可以用对象来接收
+  props: {
+    //   定义一个props属性
+    treeNode: {
+      type: Object, // 对象类型
+      required: true // 要求对方使用您的组件的时候 必须传treeNode属性 如果不传 就会报错
+    },
+    isRoot: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -2,22 +2,65 @@
  * @Author: Dabbie 2310734576@qq.com
  * @Date: 2023-01-04 10:52:22
  * @LastEditors: Dabbie 2310734576@qq.com
- * @LastEditTime: 2023-01-04 14:22:10
+ * @LastEditTime: 2023-01-04 17:32:45
  * @FilePath: \bg-system\src\views\approvals\index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>组织架构</h2>
+      <!-- 组织架构内容--头部 -->
+      <el-card class="tree-card">
+        <!-- 放置结构内容 -->
+        <TreeTools :tree-node="company" :is-root="true" />
+        <!-- 树形el-tree -->
+        <el-tree
+          :data="departs"
+          :props="defaultProps"
+          :default-expand-all="true"
+        >
+          <!-- 传入内容 插槽内容 会循环多次 有多少节点就循环多少次  -->
+          <!-- 作用于插槽 slot-scope="obj" 接收传递给插槽的数据 data：每个节点的数据对象 -->
+          <!-- tree-node就是组件中的treeNode -->
+          <TreeTools slot-scope="{ data }" :tree-node="data" />
+        </el-tree>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+import TreeTools from './components/tree-tools'
+export default {
+  components: {
+    TreeTools
+  },
+  data() {
+    return {
+      defaultProps: {
+        label: 'name'
+      },
+      // 单独定义头部
+      company: { name: 'XXX公司', manager: '负责人' },
+      // departs：组织架构
+      departs: [
+        {
+          name: '总裁办',
+          manager: '曹操',
+          children: [{ name: '董事会', manager: '曹丕' }]
+        },
+        { name: '行政部', manager: '刘备' },
+        { name: '人事部', manager: '孙权' }
+      ]
+    }
+  }
+}
 </script>
 
 <style scoped>
+.tree-card {
+  padding: 30px 140px;
+  font-size: 14px;
+}
 </style>
 
