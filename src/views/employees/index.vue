0a9d3391ac2c4dfd311ba6786529acabc7d5ec68
Dabbie <2310734576@qq.com>
2023-01-08 17:40:24 +0800

    员工列表页面的基本布局和结构

--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -2,14 +2,55 @@
  * @Author: Dabbie 2310734576@qq.com
  * @Date: 2023-01-04 10:52:22
  * @LastEditors: Dabbie 2310734576@qq.com
- * @LastEditTime: 2023-01-04 10:52:39
+ * @LastEditTime: 2023-01-08 17:37:07
  * @FilePath: \bg-system\src\views\approvals\index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>员工</h2>
+      <page-tools :show-before="true">
+        <!-- 左侧记录总记录数 -->
+        <span slot="before">共166条记录</span>
+        <!-- 右侧显示按钮 -->
+        <template slot="after">
+          <el-button size="small" type="warning">导入</el-button>
+          <el-button size="small" type="danger">导出</el-button>
+          <el-button size="small" type="primary">新增员工</el-button>
+        </template>
+      </page-tools>
+      <!-- 放置表格和分页 -->
+      <el-card>
+        <el-table border>
+          <!-- sortable 排序 -->
+          <el-table-column label="序号" sortable="" />
+          <el-table-column label="姓名" sortable="" />
+          <el-table-column label="工号" sortable="" />
+          <el-table-column label="聘用形式" sortable="" />
+          <el-table-column label="部门" sortable="" />
+          <el-table-column label="入职时间" sortable="" />
+          <el-table-column label="账户状态" sortable="" />
+          <el-table-column label="操作" sortable="" fixed="right" width="280">
+            <template>
+              <el-button type="text" size="small">查看</el-button>
+              <el-button type="text" size="small">转正</el-button>
+              <el-button type="text" size="small">调岗</el-button>
+              <el-button type="text" size="small">离职</el-button>
+              <el-button type="text" size="small">角色</el-button>
+              <el-button type="text" size="small">删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+        <!-- 分页组件 -->
+        <el-row
+          type="flex"
+          justify="center"
+          align="middle"
+          style="height: 60px"
+        >
+          <el-pagination layout="prev, pager, next" />
+        </el-row>
+      </el-card>
     </div>
   </div>
 </template>
